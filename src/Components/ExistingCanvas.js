d3da8617ad829dd802c6285281d5c1b7356b82b4
Finn Doherty <15265859+FinnDoherty@users.noreply.github.com>
2021-10-02 00:28:11 +0100

    Add copy button for easier sharing

--- src/Components/ExistingCanvas.js
+++ src/Components/ExistingCanvas.js
@@ -4,6 +4,7 @@ import DateUtils from "../Utils/DateUtils";
 import FormTab from "./FormTab";
 import ResultsTab from "./ResultsTab";
 import { collection, query, where, orderBy, onSnapshot, getDocs } from "firebase/firestore";
+import { IoCopy } from 'react-icons/io5';
 
 export default class ExistingCanvas extends Component {
   constructor(props) {
@@ -116,10 +117,38 @@ export default class ExistingCanvas extends Component {
     });
   }
 
+  canUseClipboard() {
+    return (navigator.clipboard && navigator.clipboard.writeText);
+  }
+
+  handleCopyClick() {
+    if (this.canUseClipboard()) {
+      let path = (new URL(document.URL)).pathname;
+
+      navigator.clipboard.writeText('https://datepainter.com' + path).then(() => {
+        var el = document.getElementById("copied-toast");
+        el.classList.add("visible");
+
+        setTimeout(function() {
+          el.classList.remove('visible');
+        }, 1000);
+      })
+      .catch(console.error);
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
-        <h2 className="title">{this.state.occasionName}</h2>
+        <h2 className="title">
+
+        {this.state.occasionName}
+        { this.canUseClipboard() &&
+          <IoCopy className="copy-icon" onClick={() => this.handleCopyClick()}/>
+        }
+        </h2>
+
+        <div id="copied-toast" className="copied-toast">Link copied to clipboard</div>
 
         <FormTab
           {...this.props}
